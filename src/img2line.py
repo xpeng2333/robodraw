c5ee767d35ae211a1bfcad46d50250df6c3abb0c
xiaopeng <2544627510@qq.com>
2017-06-25 16:44:53 +0800

    It's a failure.

--- src/img2line.py
+++ src/img2line.py
@@ -6,7 +6,7 @@ from pylab import *
 from skimage import io, data
 
 # 读取图片,灰度化，并转为数组
-im0 = Image.open("../data/img/girl.jpeg").convert('L')
+im0 = Image.open("../data/img/test.jpg").convert('L')
 im = array(im0)
 
 # print(abs(int(im[2, 8]) - int(im[3, 7])))
@@ -33,6 +33,7 @@ for x in h[::2]:
 
 # 确定划线方向函数,exist已存在0:无 1：左上 2：上 3：右上 4：左 x,y坐标
 # 返回0:下 1：左下 2：右下 3：右
+'''
 def direct(exist, x, y):
     rt = abs(int(im[x, y]) - int(im[x, y + 1]))
     rd = abs(int(im[x, y]) - int(im[x + 1, y + 1]))
@@ -45,43 +46,159 @@ def direct(exist, x, y):
         'rightdown': rd
     }
     return sorted(path.keys())
-
+'''
 
 # 在“点图”中连线
-for x in h[:m - 2:2]:
+for x in h[1:m - 2:2]:
     for y in k[3::2]:
+
+        rt = abs(int(im[x, y]) - int(im[x, y + 2]))
+        rd = abs(int(im[x, y]) - int(im[x + 2, y + 2]))
+        dn = abs(int(im[x, y]) - int(im[x + 2, y]))
+        ld = abs(int(im[x, y]) - int(im[x + 2, y - 2]))
+        path = {
+            'down': dn,
+            'leftdown': ld,
+            'right': rt,
+            'rightdown': rd
+        }
+        sortlist = sorted(path.items(), key=lambda item: item[1])
+
         # 左上角已连线
         if(im[x - 1, y - 1] != 255):
-            direct(1, x, y)
+            if(sortlist[0][0] != 'leftdown'):
+                if(sortlist[0][0] == 'down'):
+                    im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
+                elif(sortlist[0][0] == 'right'):
+                    im[x, y + 1] = (int(im[x, y]) + int(im[x, y + 2])) / 2
+                elif(sortlist[0][0] == 'rightdown'):
+                    im[x + 1, y + 1] = (int(im[x, y]) +
+                                        int(im[x + 2, y + 2])) / 2
+                else:
+                    pass
+            else:
+                if(sortlist[1][0] == 'down'):
+                    im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
+                elif(sortlist[1][0] == 'right'):
+                    im[x, y + 1] = (int(im[x, y]) + int(im[x, y + 2])) / 2
+                elif(sortlist[1][0] == 'rightdown'):
+                    im[x + 1, y + 1] = (int(im[x, y]) +
+                                        int(im[x + 2, y + 2])) / 2
+                else:
+                    pass
             continue
 
         # 上方已连线
-        if(im[x - 1, y] != 255):
-            direct(2, x, y)
+        elif(im[x - 1, y] != 255):
+            if(sortlist[0][0] != 'right'):
+                if(sortlist[0][0] == 'down'):
+                    im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
+                elif(sortlist[0][0] == 'leftdown'):
+                    im[x + 1, y - 1] = (int(im[x, y]) +
+                                        int(im[x + 2, y - 2])) / 2
+                elif(sortlist[0][0] == 'rightdown'):
+                    im[x + 1, y + 1] = (int(im[x, y]) +
+                                        int(im[x + 2, y + 2])) / 2
+                else:
+                    pass
+            else:
+                if(sortlist[1][0] == 'down'):
+                    im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
+                elif(sortlist[1][0] == 'leftdown'):
+                    im[x + 1, y - 1] = (int(im[x, y]) +
+                                        int(im[x + 2, y - 2])) / 2
+                elif(sortlist[1][0] == 'rightdown'):
+                    im[x + 1, y + 1] = (int(im[x, y]) +
+                                        int(im[x + 2, y + 2])) / 2
+                else:
+                    pass
             continue
 
         # 右上已连线
-        if(im[x - 1, y + 1] != 255):
-            direct(3, x, y)
+        elif(im[x - 1, y + 1] != 255):
+            if(sortlist[0][0] != 'right'):
+                if(sortlist[0][0] != 'rightdown'):
+                    if(sortlist[0][0] == 'down'):
+                        im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
+                    else:
+                        im[x + 1, y - 1] = (int(im[x, y]) +
+                                            int(im[x + 2, y - 2])) / 2
+                elif(sortlist[1][0] != 'right'):
+                    if(sortlist[1][0] != 'down'):
+                        im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
+                    else:
+                        im[x + 1, y - 1] = (int(im[x, y]) +
+                                            int(im[x + 2, y - 2])) / 2
+                else:
+                    if(sortlist[2][0] != 'down'):
+                        im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
+                    else:
+                        im[x + 1, y - 1] = (int(im[x, y]) +
+                                            int(im[x + 2, y - 2])) / 2
+            else:
+                if(sortlist[1][0] != 'rightdown'):
+                    if(sortlist[1][0] != 'down'):
+                        im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
+                    else:
+                        im[x + 1, y - 1] = (int(im[x, y]) +
+                                            int(im[x + 2, y - 2])) / 2
+                else:
+                    if(sortlist[2][0] != 'down'):
+                        im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
+                    else:
+                        im[x + 1, y - 1] = (int(im[x, y]) +
+                                            int(im[x + 2, y - 2])) / 2
             continue
 
         # 左方已连线
-        if(im[x, y - 1] != 255):
-            direct(4, x, y)
+        elif(im[x, y - 1] != 255):
+            if(sortlist[0][0] != 'down'):
+                if(sortlist[0][0] != 'leftdown'):
+                    if(sortlist[0][0] == 'right'):
+                        im[x, y + 1] = (int(im[x, y]) + int(im[x, y + 2])) / 2
+                    else:
+                        im[x + 1, y + 1] = (int(im[x, y]) +
+                                            int(im[x + 2, y + 2])) / 2
+                elif(sortlist[1][0] != 'down'):
+                    if(sortlist[1][0] != 'right'):
+                        im[x, y + 1] = (int(im[x, y]) + int(im[x, y + 2])) / 2
+                    else:
+                        im[x + 1, y + 1] = (int(im[x, y]) +
+                                            int(im[x + 2, y + 2])) / 2
+                else:
+                    if(sortlist[2][0] != 'right'):
+                        im[x, y + 1] = (int(im[x, y]) + int(im[x, y + 2])) / 2
+                    else:
+                        im[x + 1, y + 1] = (int(im[x, y]) +
+                                            int(im[x + 2, y + 2])) / 2
+            else:
+                if(sortlist[1][0] != 'leftdown'):
+                    if(sortlist[1][0] != 'right'):
+                        im[x, y + 1] = (int(im[x, y]) + int(im[x, y + 2])) / 2
+                    else:
+                        im[x + 1, y + 1] = (int(im[x, y]) +
+                                            int(im[x + 2, y + 2])) / 2
+                else:
+                    if(sortlist[2][0] != 'right'):
+                        im[x, y + 1] = (int(im[x, y]) + int(im[x, y + 2])) / 2
+                    else:
+                        im[x + 1, y + 1] = (int(im[x, y]) +
+                                            int(im[x + 2, y + 2])) / 2
+
             continue
 
         # 孤立点
-        sortlist = direct(0, x, y)
-        if(min(sortlist) == 'down'):
-            print(min(sortlist))
-            im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
-        if(min == 'leftdown'):
-            im[x + 1, y - 1] = (int(im[x, y]) + int(im[x - 2, y - 2])) / 2
-        if(min == 'right'):
-            im[x, y + 1] = (int(im[x, y]) + int(im[x, y + 2])) / 2
-        if(min == 'rightdown'):
-            im[x + 1, y + 1] = (int(im[x, y]) + int(im[x + 2, y + 2])) / 2
- #   print(min(sortlist)
+        else:
+            if(sortlist[0][0] == 'down'):
+                im[x + 1, y] = (int(im[x, y]) + int(im[x + 2, y])) / 2
+            elif(sortlist[0][0] == 'leftdown'):
+                im[x + 1, y - 1] = (int(im[x, y]) + int(im[x + 2, y - 2])) / 2
+            elif(sortlist[0][0] == 'right'):
+                im[x, y + 1] = (int(im[x, y]) + int(im[x, y + 2])) / 2
+            elif(sortlist[0][0] == 'rightdown'):
+                im[x + 1, y + 1] = (int(im[x, y]) + int(im[x + 2, y + 2])) / 2
+            else:
+                pass
 
 imshow(im)
 # imshow(matrix)
