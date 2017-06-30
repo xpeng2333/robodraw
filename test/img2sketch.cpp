4d9a04ce67524429aef815b9e6abf1ea80a7e339
xiaopeng <2544627510@qq.com>
2017-06-30 14:21:38 +0800

    A success in picture processing.

---
+++ test/img2sketch.cpp
@@ -0,0 +1,35 @@
+#include <math.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+
+int main() {
+    Mat src = imread("../data/img/girl.jpeg", 1);
+    int width = src.cols;
+    int heigh = src.rows;
+    Mat gray0, gray1;
+    //去色
+    cvtColor(src, gray0, CV_BGR2GRAY);
+    //反色
+    addWeighted(gray0, -1, NULL, 0, 255, gray1);
+    //高斯模糊,高斯核的Size与最后的效果有关
+    GaussianBlur(gray1, gray1, Size(51, 51), 0);
+
+    //融合：颜色减淡
+    Mat img(gray1.size(), CV_8UC1);
+    for (int y = 0; y < heigh; y++) {
+
+        uchar *P0 = gray0.ptr<uchar>(y);
+        uchar *P1 = gray1.ptr<uchar>(y);
+        uchar *P = img.ptr<uchar>(y);
+        for (int x = 0; x < width; x++) {
+            int tmp0 = P0[x];
+            int tmp1 = P1[x];
+            P[x] = (uchar)min((tmp0 + (tmp0 * tmp1) / (256 - tmp1)), 255);
+        }
+    }
+    imshow("素描", img);
+    waitKey();
+    //   imwrite("D:/素描.jpg",img);
+}
