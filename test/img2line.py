3d9921dac03ee1cab8bf4b4cb4bde6b90688ab74
xiaopeng <2544627510@qq.com>
2017-07-30 13:09:30 +0800

    a little change of path

--- test/img2line.py
+++ test/img2line.py
@@ -6,7 +6,7 @@ from pylab import *
 from skimage import io, data
 
 # 读取图片,灰度化，并转为数组
-im0 = Image.open("../data/img/test.jpg").convert('L')
+im0 = Image.open("../data/img/sketch.jpg")
 im = array(im0)
 
 # print(abs(int(im[2, 8]) - int(im[3, 7])))
@@ -20,7 +20,7 @@ k = range(n - 2)
 # matrix = np.array([[255 for i in range(n)] for i in range(m)])
 limit = 255
 
-gray()
+# gray()
 
 
 # 将图像处理成“点图”，五点取样
