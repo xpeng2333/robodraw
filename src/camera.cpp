17a8f2846c53fd0830b5fcec2e51560de94a7a42
xiaopeng <2544627510@qq.com>
2017-08-22 22:24:24 +0800

    take a photo

---
+++ src/camera.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <highgui.h>
+#include <string.h>
+
+int main() {
+    IplImage *pImg = NULL;
+    CvCapture *cap = cvCaptureFromCAM(1);
+    char filename[12] = "../data/img/cam2img.jpg";
+
+    pImg = cvQueryFrame(cap);
+    cvSaveImage(filename, pImg);
+    cvXorS(pImg, cvScalarAll(255),
+           pImg); //将拍到的图像反色（闪一下形成拍照效果）
+    cvShowImage("Camera", pImg);
+    cvWaitKey(200); //反色图像显示ms
+    cvShowImage("Camera", pImg);
+
+    cvReleaseCapture(&cap);
+    return 0;
+}
