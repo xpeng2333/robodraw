dca1ead3dde6573a897c775b8b0bc5131032d65f
xiaopeng <2544627510@qq.com>
2017-08-26 15:37:28 +0800

    a change of path

--- src/send_PC.py
+++ src/send_PC.py
@@ -6,12 +6,12 @@ SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 建立连接:
-s.connect(('10.42.0.126', 9999))
+s.connect(('192.168.43.101', 9999))
 # 接收欢迎消息:
 print s.recv(SIZE)
 s.send('begin to send')
 print 'sending, please wait for a second ...'
-with open('../data/img/result.jpeg', 'rb') as f:
+with open('../data/text/out_step.txt', 'rb') as f:
     for data in f:
         s.send(data)
 print 'sended !'
